4cbce107fbd394db21b1c14f3a5b20ecf37ddb21
santrenan <157501843+santrenan@users.noreply.github.com>
2025-07-06 11:26:13 -0300

    Update movie_data_creator.py

--- movie_data_creator.py
+++ movie_data_creator.py
@@ -4,19 +4,20 @@ import csv
 movies = []
 
 # Alterar nome do arquivo na hora de rodar
-with open("movies_list.csv", 'r', encoding='utf-8') as csv_file: 
+file = "movies_list.csv"
+with open(file, "r", encoding="utf-8") as csv_file: 
     reader = csv.DictReader(csv_file)
     for row in reader: # Cada row é um Dict
         movies.append(row)
     
 
 # Cria um Insert para add no Banco de Dados (MySQL)
-with open('insert_movies.sql', 'w', encoding='utf-8') as sqlfile:
+with open("insert_movies.sql", "w", encoding="utf-8") as sqlfile:
     for movie in movies:
-        date = movie['Date']
-        name = movie['Name'].replace("'", "''")
-        year = movie['Year']
-        uri = movie['Letterboxd URI']
+        date = movie["Date"]
+        name = movie["Name"].replace("'", "''")
+        year = movie["Year"]
+        uri = movie["Letterboxd URI"]
 
         sql = f"INSERT INTO movies_data (date, name, year, uri) VALUES ('{date}', '{name}', {year}, '{uri}');\n"
         sqlfile.write(sql)
